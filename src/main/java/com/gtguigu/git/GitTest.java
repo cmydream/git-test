67d3b8afda58f96bbbd32e0db0023ff71af19882
cmy <cuimy_work@163.com>
2023-02-23 21:34:28 +0800

    master3

--- src/main/java/com/gtguigu/git/GitTest.java
+++ src/main/java/com/gtguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix1");
         System.out.println("hello hot-fix1111111");
+        System.out.println("hello hot-fix2222222");
     }
 }
 /**
