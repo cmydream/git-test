06c088abc9e6949686e16352a0a995b1e8230a29
cmy <cuimy_work@163.com>
2023-02-23 20:24:15 +0800

    first commit

---
+++ src/main/java/com/gtguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.gtguigu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
+/**
+ * @description TODO
+ * @author cmy
+ * @date 2023/2/23 20:19
+ * @version 1.0
+ */
